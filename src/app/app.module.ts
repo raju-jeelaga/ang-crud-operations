d2da94504fb1a5cea0823890f3400e3f06535612
Raju Jeelaga <rajujeelaga@gmail.com>
2020-07-05 11:52:44 +0530

    Form is added
    Form is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { IndexComponent } from "./index/index.component";
+
+import { DashboardModule } from "./dashboard/dashboard.module";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, IndexComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    DashboardModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
